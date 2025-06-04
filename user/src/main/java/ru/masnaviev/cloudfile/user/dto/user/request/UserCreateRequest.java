3baf1558603c3912a33441e91cfab509f2135c3b
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-04 15:17:30 +0300

    feat(user): add UserCreateRequest validation

--- user/src/main/java/ru/masnaviev/cloudfile/user/dto/user/request/UserCreateRequest.java
+++ user/src/main/java/ru/masnaviev/cloudfile/user/dto/user/request/UserCreateRequest.java
@@ -1,6 +1,18 @@
 package ru.masnaviev.cloudfile.user.dto.user.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserCreateRequest {
+    @NotBlank(message = "Username must not be empty")
+    @Size(min = 8, max = 50)
     private String username;
+
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 8, max = 100, message = "Password length must be between 8 and 100")
     private String password;
 }
