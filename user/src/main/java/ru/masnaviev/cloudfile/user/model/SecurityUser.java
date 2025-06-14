78d5683eeda851f50fa286480b6b3d5de4433a52
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-14 13:23:35 +0300

    feat(user): add security

---
+++ user/src/main/java/ru/masnaviev/cloudfile/user/model/SecurityUser.java
@@ -0,0 +1,28 @@
+package ru.masnaviev.cloudfile.user.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> user.getRoles().toString());//TODO
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ user/src/main/java/ru/masnaviev/cloudfile/user/service/SecurityUserService.java
@@ -0,0 +1,22 @@
+package ru.masnaviev.cloudfile.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ru.masnaviev.cloudfile.user.model.SecurityUser;
+import ru.masnaviev.cloudfile.user.repository.UserRepository;
+
+
+@Component
+@RequiredArgsConstructor
+public class SecurityUserService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new SecurityUser(repository.findByUsername(username));
+    }
+}
