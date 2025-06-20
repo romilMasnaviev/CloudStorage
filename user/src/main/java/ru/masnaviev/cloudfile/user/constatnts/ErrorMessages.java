83d3fd242b5cc8e3e12a5cf6212ec603be39c970
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-20 13:09:07 +0300

    test(user): add AuthController unit tests

--- user/src/main/java/ru/masnaviev/cloudfile/user/constatnts/ErrorMessages.java
+++ user/src/main/java/ru/masnaviev/cloudfile/user/constatnts/ErrorMessages.java
@@ -8,6 +8,11 @@ public class ErrorMessages {
 
     //Validation
     public static final String UNDEFINED_VALIDATION_ERROR = "";
+    public static final String USERNAME_MUST_NOT_BE_EMPTY = "Username must not be empty";
+    public static final String USERNAME_LENGTH_BETWEEN_8_50 = "Username length must be between 8 and 50";
+    public static final String PASSWORD_MUST_NOT_BE_EMPTY = "Password must not be empty";
+    public static final String PASSWORD_LENGTH_BETWEEN_8_100 = "Username length must be between 8 and 50";
+
 
     //Other
     public static final String USER_ALREADY_EXISTS = "User with this username already exists";
