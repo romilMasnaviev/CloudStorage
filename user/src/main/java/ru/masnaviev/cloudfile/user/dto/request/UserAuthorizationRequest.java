f48e9ad1cfdb43c60f43290107dc727c806d70a7
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-20 13:12:13 +0300

    refactor(user): add validation exception message constants

--- user/src/main/java/ru/masnaviev/cloudfile/user/dto/request/UserAuthorizationRequest.java
+++ user/src/main/java/ru/masnaviev/cloudfile/user/dto/request/UserAuthorizationRequest.java
@@ -3,12 +3,14 @@ package ru.masnaviev.cloudfile.user.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import static ru.masnaviev.cloudfile.user.constatnts.ErrorMessages.*;
+
 public record UserAuthorizationRequest(
-        @NotBlank(message = "Username must not be empty")
-        @Size(min = 8, max = 50, message = "Username length must be between 8 and 50")
+        @NotBlank(message = USERNAME_MUST_NOT_BE_EMPTY)
+        @Size(min = 8, max = 50, message = USERNAME_LENGTH_BETWEEN_8_50)
         String username,
 
-        @NotBlank(message = "Password must not be empty")
-        @Size(min = 8, max = 100, message = "Password length must be between 8 and 100")
+        @NotBlank(message = PASSWORD_MUST_NOT_BE_EMPTY)
+        @Size(min = 8, max = 100, message = PASSWORD_LENGTH_BETWEEN_8_100)
         String password) {
 }
