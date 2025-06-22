28e630303e67148a77292cad3ff5429d8843447b
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-22 15:08:43 +0300

    feat(user): add logging controller methods via Aspect

--- user/src/main/java/ru/masnaviev/cloudfile/user/config/local/LocalResolverConfiguration.java
+++ user/src/main/java/ru/masnaviev/cloudfile/user/config/local/LocalResolverConfiguration.java
@@ -1,4 +1,4 @@
-package ru.masnaviev.cloudfile.user.config;
+package ru.masnaviev.cloudfile.user.config.local;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
