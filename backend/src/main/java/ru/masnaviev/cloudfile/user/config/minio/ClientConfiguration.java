6da45d0e1562d14bc89066db1c6355ab482476c7
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-06-27 17:55:26 +0300

    refactor(project): transferring project to monolithic architecture. Add minio configuration in application properties & ClientConfiguration

---
+++ backend/src/main/java/ru/masnaviev/cloudfile/user/config/minio/ClientConfiguration.java
@@ -0,0 +1,47 @@
+package ru.masnaviev.cloudfile.user.config.minio;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.errors.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+@RequiredArgsConstructor
+class ClientConfiguration {
+
+    private final Environment env;
+
+    @Bean
+    MinioClient minioClient() {
+        MinioClient client = MinioClient.builder()
+                .endpoint(env.getProperty("minio.endpoint"))
+                .credentials(env.getProperty("minio.username"), env.getProperty("minio.password"))
+                .build();
+
+        createBucketForFiles(env.getProperty("minio.bucket.name"), client);
+
+        return client;
+    }
+
+    private void createBucketForFiles(String bucketName, MinioClient client) {
+        try {
+            if (!client.bucketExists(BucketExistsArgs.builder()
+                    .bucket(bucketName).build())) {
+                client.makeBucket(MakeBucketArgs.builder()
+                        .bucket(bucketName).build());
+            }
+        } catch (ServerException | InsufficientDataException | ErrorResponseException | IOException |
+                 NoSuchAlgorithmException | InvalidKeyException | InvalidResponseException | XmlParserException |
+                 InternalException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
