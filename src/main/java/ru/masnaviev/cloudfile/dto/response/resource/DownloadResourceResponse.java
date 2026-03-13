d2d4f806d5c09100ef9c9bb13106e89adb6f3992
romilMasnaviev <masnaviev.romil@mail.ru>
2026-03-13 19:31:10 +0300

    refactor(storage): refactor NormalizedResourceData

--- src/main/java/ru/masnaviev/cloudfile/dto/response/resource/DownloadResourceResponse.java
+++ src/main/java/ru/masnaviev/cloudfile/dto/response/resource/DownloadResourceResponse.java
@@ -3,11 +3,12 @@ package ru.masnaviev.cloudfile.dto.response.resource;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.core.io.InputStreamResource;
+import ru.masnaviev.cloudfile.util.NormalizedResourceData;
 
 @AllArgsConstructor
 @Getter
 public class DownloadResourceResponse {
     String resourceName;
     InputStreamResource resource;
-    ResourceType resourceType;
+    NormalizedResourceData.ResourceType resourceType;
 }
